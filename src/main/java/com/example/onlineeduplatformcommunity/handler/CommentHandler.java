b344e55e487194c55ea5cf6e500fce774163c2a4
diqksrk <diqksrk123@naver.com>
2022-04-26 21:37:09 +0900

    comment

--- src/main/java/com/example/onlineeduplatformcommunity/handler/CommentHandler.java
+++ src/main/java/com/example/onlineeduplatformcommunity/handler/CommentHandler.java
@@ -1,18 +1,57 @@
 package com.example.onlineeduplatformcommunity.handler;
 
+import com.example.onlineeduplatformcommunity.model.Comment;
+import com.example.onlineeduplatformcommunity.service.CommentService;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
-public interface CommentHandler {
+public class CommentHandler {
+    private CommentService commentService;
 
-    Mono<ServerResponse> getComment(ServerRequest serverRequest);
+    public CommentHandler(CommentService commentService) {
+        this.commentService = commentService;
+    }
 
-    Mono<ServerResponse> createComment(ServerRequest serverRequest);
+    public Mono<ServerResponse> createComment(ServerRequest serverRequest) {
+        int articleId = Integer.parseInt(serverRequest.pathVariable("articleId"));
+        Mono<Comment> commentMono = serverRequest.bodyToMono(Comment.class);
 
-    Mono<ServerResponse> blockComment(ServerRequest serverRequest);
+//         Mono<Comment> responseCommentMono = commentMono.flatMap(comment -> {
+//            return commentService.createComment(comment);
+//        });
 
-    Mono<ServerResponse> getCommentList(ServerRequest serverRequest);
+//        return ServerResponse.ok()
+//                            .contentType(MediaType.APPLICATION_JSON)
+//                            .body(responseCommentMono, Comment.class);
+        return null;
+    }
+
+    public Mono<ServerResponse> getComment(ServerRequest serverRequest) {
+        int articleId = Integer.parseInt(serverRequest.pathVariable("articleId"));
+        int commentId = Integer.parseInt(serverRequest.pathVariable("commentId"));
+
+        Mono<Comment> responseComment = commentService.getComment(articleId, commentId);
+
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(responseComment, Comment.class);
+    }
+
+    //    Mono<ServerResponse> blockComment(ServerRequest serverRequest);
+//
+    public Mono<ServerResponse> getCommentList(ServerRequest serverRequest) {
+        int articleId = Integer.parseInt(serverRequest.pathVariable("articleId"));
+
+        Flux<Comment> articleFlux = commentService.getCommentList(articleId);
+
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(articleFlux, Comment.class);
+    }
 }
+
--- src/main/java/com/example/onlineeduplatformcommunity/model/Comment.java
+++ src/main/java/com/example/onlineeduplatformcommunity/model/Comment.java
@@ -1,51 +1,23 @@
 package com.example.onlineeduplatformcommunity.model;
 
+import lombok.*;
+import org.springframework.data.relational.core.mapping.Table;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Table(value = "comment")
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Comment {
 
-    private final int commentId;
-    private final int articleId;
-    private final int userId;
-    private final String comment;
+    @Id @GeneratedValue
+    private int commentId;
+    private int articleId;
+    private int userId;
+    private String comment;
     private boolean blockYn;
     private String blockComment;
-
-    public Comment(int commentId, int articleId, int userId, String comment) {
-        this.commentId = commentId;
-        this.articleId = articleId;
-        this.userId = userId;
-        this.comment = comment;
-        this.blockYn = false;
-    }
-
-    public int getCommentId() {
-        return commentId;
-    }
-
-    public int getArticleId() {
-        return articleId;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public boolean getBlockYn() {
-        return blockYn;
-    }
-
-    public void setBlockYn(boolean blockYn) {
-        this.blockYn = blockYn;
-    }
-
-    public String getBlockComment() {
-        return blockComment;
-    }
-
-    public void setBlockComment(String blockComment) {
-        this.blockComment = blockComment;
-    }
 }
